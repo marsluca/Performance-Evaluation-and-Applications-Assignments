b631fc4dbeb3588247e3949651bba55603f8e9ba
Patatone <34103646+Patatone@users.noreply.github.com>
2021-12-02 16:56:27 +0100

    L02 done

--- L01 - Basic Performance Indices/L01_Solution.m
+++ L01 - Basic Performance Indices/L01_Solution.m
@@ -1,52 +1,51 @@
 clear variables;
 clc;
 
-filename = 'DataSet1.csv';
-run(filename);
-filename = 'DataSet2.csv';
-run(filename);
+for i = 1:2    
+    run(sprintf('DataSet%d.csv', i));
+end
 
 function run(filename)
-% Arrival time, Departure time
-records = readmatrix(filename);
-fprintf('File name: %s\n', filename);
+    fprintf('File name: %s\n', filename);
+    % Arrival time, Departure time
+    records = readmatrix(filename);
+    % Number of arrivals
+    A = size(records, 1);
+    % Completions
+    C = size(records, 1);
 
-% Number of arrivals
-A = size(records, 1);
-% Completions
-C = size(records, 1);
+    % Time = last departure time
+    T = records(C, 2);
 
-% Time = last departure time
-T = records(C, 2);
+    % Arrival Rate (Lambda=A/T)
+    Lambda = A/T;
+    fprintf('Arrival rate: %f\n', Lambda);
 
-% Arrival Rate (Lambda=A/T)
-Lambda = A/T;
-fprintf('Arrival rate: %f\n', Lambda);
+    % Throughput (X=C/T)
+    X = C/T;
+    fprintf('Throughput: %f\n', X);
 
-% Throughput (X=C/T)
-X = C/T;
-fprintf('Throughput: %f\n', X);
+    % Response time = difference between the
+    % departure time and the arrival time
+    % All the elements of the second column - All the elements of the first column
+    r_i = records(:,2)-records(:,1);
 
-% Response time = difference between the
-% departure time and the arrival time
-r_i = records(:,2)-records(:,1);
+    % Sum of all response time r_i
+    W = sum(r_i);
+    fprintf('W: %d\n', W);
 
-% Sum of all response time r_i
-W = sum(r_i);
-fprintf('W: %d\n', W);
+    % Average Number of Jobs in the system
+    % (Little's law rielaboration)
+    N = W/T;
+    fprintf('Average Number of Jobs: %f\n', N);
 
-% Average Number of Jobs in the system
-% (Little's law rielaboration)
-N = W/T;
-fprintf('Average Number of Jobs: %d\n', N);
+    % Average Response Time
+    % (Little's law rielaboration)
+    R = W/C;
+    fprintf('Average Response Time: %f\n', R);
 
-% Average Response Time
-% (Little's law rielaboration)
-R = W/C;
-fprintf('Average Response Time: %f\n', R);
- 
-% Probability response time < threshold
-p = sum([r_i<1, r_i<10, r_i<50]) / C;
-fprintf('P(R<1):%f P(R<10):%f P(R<50):%f\n', p(1), p(2), p(3));
-fprintf('\n');
+    % Probability response time < threshold
+    p = sum([r_i<1, r_i<10, r_i<50]) / C;
+    fprintf('P(R<1):%f P(R<10):%f P(R<50):%f\n', p(1), p(2), p(3));
+    fprintf('\n');
 end
---
+++ README.md
@@ -0,0 +1,2 @@
+# Performance-Evaluation-and-Applications-Assignments
+Performance Evaluation and Applications assignment solutions for the A.Y. 2021-2022
